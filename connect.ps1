b76db4aceb0bfd95359a3719badd5f03ac14f47c
jing-tw <mqjing@gmail.com>
2015-03-22 18:26:35 +0800

    add GustOS and cd drive to fuel master

---
+++ connect.ps1
@@ -0,0 +1,9 @@
+
+$esxi_host_ip='172.16.219.130'
+$user='root'
+$passwd='1234567'
+set-executionpolicy remotesigned
+. "C:\Program Files (x86)\VMware\Infrastructure\vSphere PowerCLI\Scripts\Initialize-PowerCLIEnvironment.ps1"
+
+Connect-VIServer -Server $esxi_host_ip -User $user -Password $passwd
+
--- init-nodes.ps1
+++ init-nodes.ps1
@@ -1,14 +1,5 @@
 
 
-$esxi_host_ip='172.16.219.130'
-$user='root'
-$passwd='1234567'
-set-executionpolicy remotesigned
-. "C:\Program Files (x86)\VMware\Infrastructure\vSphere PowerCLI\Scripts\Initialize-PowerCLIEnvironment.ps1"
-
-Connect-VIServer -Server $esxi_host_ip -User $user -Password $passwd
-
-
 
 #Step 1: Create port-group
 
@@ -25,12 +16,12 @@ get-virtualportgroup
 
 
 #Step 2: Create Fuel Master VM with hardware configuration
-$host_name='fuel-master'
-$hddGB_capacity=5
+$node_name='fuel-master'
+$hddGB_capacity=30
 $memoryGB_capacity=2
 $network_name='Admin-PXE'
-new-vm -vmhost $esxi_host_ip -name $host_name -diskGB $hddGB_capacity -memoryGB $memoryGB_capacity -networkname $network_name -DiskStorageFormat thin 
-
+$objvm=new-vm -vmhost $esxi_host_ip -name $node_name -diskGB $hddGB_capacity -memoryGB $memoryGB_capacity -networkname $network_name -guestId centos64Guest -DiskStorageFormat thin 
+new-cddrive -vm $objvm
 
 #Step 3: Create 3 Slaves for controller, cinder, compute nodes with hardware configuration
 $hddGB_capacity=5
@@ -38,7 +29,7 @@ $memoryGB_capacity=4
 
 $node_array=@('slave-3nics-controller', 'slave-3nics-cinder', 'slave-3nics-compute')
 foreach ($node_name in $node_array) {
-	$objVM=new-vm -vmhost $esxi_host_ip -name $node_name -diskGB $hddGB_capacity -memoryGB $memoryGB_capacity -networkname 'Admin-PXE' -DiskStorageFormat thin 
+	$objVM=new-vm -vmhost $esxi_host_ip -name $node_name -diskGB $hddGB_capacity -memoryGB $memoryGB_capacity -networkname 'Admin-PXE' -guestId centos64Guest -DiskStorageFormat thin 
 	new-networkadapter -vm $objVM -networkname "VM Network" -wakeonlan -startconnected
 	new-networkadapter -vm $objVM -networkname "VM Network" -wakeonlan -startconnected	
 }
